d292d6262b85ecc764fa9448e5f62473c0926f9b
bradzara <bradzara93@gmail.com>
2024-08-08 12:04:59 -0700

    JS deliberate practice

---
+++ practice12.js
@@ -0,0 +1,38 @@
+// Write a program that uses a variable to store a number, then prints that number plus 10.
+
+var number = 10;
+console.log(number + 10);
+
+// Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+var word = "word";
+console.log(word.toUpperCase());
+
+// Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+var num1 = 5;
+var num2 = 6;
+console.log(num1 + num2)
+
+// Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+var word = 'variable';
+console.log(word.split('').reverse().join(''));
+
+// Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+var word1 = "one";
+var word2 = "two";
+console.log(word1.toUpperCase() + word2.toUpperCase());
+
+// Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+var word = "bread";
+console.log(word.length);
+
+// Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+
+var number = 6.54;
+console.log(parseInt(number));
+
+
